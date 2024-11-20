7d12b42c639ea81bea6e66dfdf6371737f81b7ab
Jamie Singleton <43870204+JamieS00@users.noreply.github.com>
2024-11-19 20:04:32 -0500

    added typerwriter plugin and form handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+let generatePoem = (event) => {
+  event.preventDefault(); //stops page from reloading
 
+  //typerwriter will show in the poem element
+  new Typewriter("#poem", {
+    strings: ["TESTING...POEM WILL DISPLAY HERE"],
+    autoStart: true,
+    cursor: null,
+    delay: 1,
+  });
+};
+
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
