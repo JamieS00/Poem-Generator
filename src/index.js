cb5b5d377fd5cfe34621c2d38e6785fbf3576a9d
Jamie Singleton <43870204+JamieS00@users.noreply.github.com>
2024-11-21 12:47:07 -0500

    added API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-let generatePoem = (event) => {
-  event.preventDefault(); //stops page from reloading
+let displayPoem = (response) => {
+  // let poemResponse = response.data.answer;
 
   //typerwriter will show in the poem element
   new Typewriter("#poem", {
-    strings: ["TESTING...POEM WILL DISPLAY HERE"],
+    strings: response.data.answer,
     autoStart: true,
-    cursor: null,
-    delay: 1,
+    cursor: "",
+    delay: 30,
   });
+};
+
+let generatePoem = (event) => {
+  event.preventDefault(); //stops page from reloading
+
+  let searchInput = document.querySelector("#search-input");
+  // console.log(searchInput.value); //testing
+
+  let poem = document.querySelector("#poem");
+  poem.innerHTML = `⏳ Generating poem about ${searchInput.value} please wait...`;
+
+  let apiKey = "4ddbb61eb5o419b8d734a63d7f1t0b56";
+  let prompt = `Generate poem about ${searchInput.value}`;
+  let context =
+    " You're a poem expert who loves to write poems! Your goal is to create a short poem";
+
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  //API Call
+  console.log("making API call");
+  axios.get(apiURL).then(displayPoem);
 };
 
 let poemFormElement = document.querySelector("#poem-form");
